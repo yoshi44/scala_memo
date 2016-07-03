b6bc0d9611a5525dea94a42f848866ebbe61c807
yoshi <yoshinihon@yoshis-MacBook-Air.local>
2016-07-04 06:25:57 +0900

    [branch_name] add implicit val

--- src/main/scala/pack_implicit/ImplicitValueTest.scala
+++ src/main/scala/pack_implicit/ImplicitValueTest.scala
@@ -2,9 +2,21 @@ package pack_implicit
 
 object ImplicitValueTest extends App{
 
-  implicit val i = 1
+  implicit val i1 = 1
+  implicit val i2 = 2L
+  implicit val i3 = "stringVal3"
 
-  def f(implicit i: Int) = "Hello" + i
+  def f1(implicit i: Int) = "Hello" + i
 
-  println(f) // Hello1
+  println(f1) // Hello1
+
+  def f2(implicit i: Long) = "Hello" + i
+  println(f2) // Hello2
+
+  def f3(implicit i: String) = "Hello" + i
+  println(f3) // HellostringVal3
+
+  def f4(implicit i: Double) = "Hello" + i
+  //println(f4) error
+
 }
