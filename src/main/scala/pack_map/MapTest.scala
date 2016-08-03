d1e23f6cfb49958e84cc8bc0053ee2e0129ab9ff
yoshiVirtualBox <yoshivirtualbox@yoshiVirtualBoxs-MacBook-Pro.local>
2016-08-04 05:32:59 +0900

    [branch_name] filter sample

--- src/main/scala/pack_map/MapTest.scala
+++ src/main/scala/pack_map/MapTest.scala
@@ -10,6 +10,11 @@ object MapTest extends App{
   val resultaaaClassList = aaaClassList.map(a => a.aaa)
   println(resultaaaClassList) //List(11, 22, 33)
 
+  val t = List((Some(111), Some(222)), (Some(333), Some(444)), (None, None), (Some(555), Some(666)))
+  println("t1==="+t)
+  println("t2==="+t.filter(_._2 != None)) // List((Some(111),Some(222)), (Some(333),Some(444)), (Some(555),Some(666)))
+  println("t3==="+t.filter(_._2.isDefined)) // List((Some(111),Some(222)), (Some(333),Some(444)), (Some(555),Some(666)))
+
 }
 
 case class AAA(aaa: Int, bbb: Int) {
